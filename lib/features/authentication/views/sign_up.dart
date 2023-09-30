b6da9bdbdfd37fed64bfbfd756b7dde38243e0d6
MrazTevin <millatevin@gmail.com>
2023-09-30 14:06:23 +0300

    feat: create signup page

---
+++ lib/features/authentication/views/sign_up.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthSignUP extends StatefulWidget {
+  const AuthSignUP({Key? key}) : super(key: key);
+
+  @override
+  _AuthSignUPState createState() => _AuthSignUPState();
+}
+
+class _AuthSignUPState extends State<AuthSignUP> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
