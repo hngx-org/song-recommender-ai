3b77f4f2786158f191ab789068d19800f42f9733
MrazTevin <millatevin@gmail.com>
2023-10-01 18:46:16 +0300

    feat: add content to signup page

--- lib/features/authentication/views/sign_up.dart
+++ lib/features/authentication/views/sign_up.dart
@@ -10,6 +10,388 @@ class AuthSignUP extends StatefulWidget {
 class _AuthSignUPState extends State<AuthSignUP> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Column(
+            children: [
+              Text(
+                "􀐿 􀙇 􀛨",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Text(
+                "9:41",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+          Stack(
+            children: [
+              Container(
+                width: 7.097500324249268,
+                height: 15.84000015258789,
+              ),
+              Container(
+                width: 24,
+                height: 24,
+              ),
+            ],
+          ),
+          Stack(
+            children: [
+              Stack(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: 18.669641494750977,
+                        height: 17.833019256591797,
+                      ),
+                      Container(
+                        width: 15.303778648376465,
+                        height: 16.67475128173828,
+                      ),
+                      Container(
+                        width: 7.349545955657959,
+                        height: 11.83403205871582,
+                      ),
+                      Container(
+                        width: 8.17718505859375,
+                        height: 13.189891815185547,
+                      ),
+                      Container(
+                        width: 5.843785762786865,
+                        height: 8.427390098571777,
+                      ),
+                      Container(
+                        width: 8.17718505859375,
+                        height: 13.176136016845703,
+                      ),
+                      Container(
+                        width: 10.432770729064941,
+                        height: 12.299346923828125,
+                      ),
+                      Container(
+                        width: 15.789054870605469,
+                        height: 16.703317642211914,
+                      ),
+                      Stack(
+                        children: [
+                          Container(
+                            width: 11.684926986694336,
+                            height: 16.732606887817383,
+                          ),
+                          Container(
+                            width: 3.9737703800201416,
+                            height: 6.919328689575195,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Text(
+                "Hello there",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            "Please enter you email & password to create an account ",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          Stack(
+            children: [
+              Text(
+                "Your Email",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                "someuser@gmail.com",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Container(
+                width: 378,
+                height: 0,
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: 17.333332061767578,
+                    height: 13,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(2),
+                    ),
+                  ),
+                  Image.asset(
+                    "assets/Vector 3.png",
+                    width: 17.333332061767578,
+                    height: 4.3333330154418945,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Stack(
+            children: [
+              Text(
+                "Create Password",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Container(
+                width: 378,
+                height: 0,
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: 23.833328247070312,
+                    height: 20.583332061767578,
+                  ),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    width: 7,
+                    height: 7,
+                  ),
+                  Container(
+                    width: 7,
+                    height: 7,
+                  ),
+                  Container(
+                    width: 7,
+                    height: 7,
+                  ),
+                  Container(
+                    width: 7,
+                    height: 7,
+                  ),
+                  Container(
+                    width: 7,
+                    height: 7,
+                  ),
+                  Container(
+                    width: 7,
+                    height: 7,
+                  ),
+                  Container(
+                    width: 7,
+                    height: 7,
+                  ),
+                  Container(
+                    width: 7,
+                    height: 7,
+                  ),
+                  Container(
+                    width: 7,
+                    height: 7,
+                  ),
+                  Container(
+                    width: 7,
+                    height: 7,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Stack(
+            children: [
+              Text(
+                "Already have an account?",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                "Sign In",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          Stack(
+            children: [
+              Container(
+                width: 117,
+                height: 0,
+              ),
+              Container(
+                width: 117,
+                height: 0,
+              ),
+              Text(
+                "or continue with",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Stack(
+                children: [
+                  Container(
+                    width: 110,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(17),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            width: 26,
+                            height: 30,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    width: 110,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(17),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Stack(
+                            children: [
+                              Container(
+                                width: 30,
+                                height: 29.817768096923828,
+                              ),
+                              Container(
+                                width: 12.832032203674316,
+                                height: 24.140625,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    width: 110,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(17),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        width: 13.20000171661377,
+                        height: 12.91874885559082,
+                      ),
+                      Container(
+                        width: 21.387493133544922,
+                        height: 11.13749885559082,
+                      ),
+                      Container(
+                        width: 6.056249141693115,
+                        height: 12.350000381469727,
+                      ),
+                      Container(
+                        width: 21.487504959106445,
+                        height: 11.13749885559082,
+                      ),
+                      Container(
+                        width: 27.4999942779541,
+                        height: 27.4999942779541,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Stack(
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3),
+                  color: Color(0xff2111ad),
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: 5.958334445953369,
+                    height: 7.041665077209473,
+                  ),
+                ],
+              ),
+              Text(
+                "I agree to Chatbot_AI  Public Agreement, Terms, & Privacy Policy.",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          Stack(
+            children: [
+              Container(
+                width: 378,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(17),
+                  color: Color(0xff2111ad),
+                ),
+              ),
+              Text(
+                "Continue",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
