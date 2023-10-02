a4f86adaa6b14a41e983f7271f01d5ef035d742f
MrazTevin <millatevin@gmail.com>
2023-10-02 07:34:13 +0300

    feat: position texts accordingly

--- lib/features/authentication/views/sign_up.dart
+++ lib/features/authentication/views/sign_up.dart
@@ -13,7 +13,7 @@ class _AuthSignUPState extends State<AuthSignUP> {
     return Scaffold(
       body: Column(
         children: [
-          Column(
+          const Column(
             children: [
               Text(
                 "􀐿 􀙇 􀛨",
@@ -31,63 +31,63 @@ class _AuthSignUPState extends State<AuthSignUP> {
               ),
             ],
           ),
-          Stack(
+          const Stack(
             children: [
-              Container(
+              SizedBox(
                 width: 7.097500324249268,
                 height: 15.84000015258789,
               ),
-              Container(
+              SizedBox(
                 width: 24,
                 height: 24,
               ),
             ],
           ),
-          Stack(
+          const Stack(
             children: [
               Stack(
                 children: [
                   Stack(
                     children: [
-                      Container(
+                      SizedBox(
                         width: 18.669641494750977,
                         height: 17.833019256591797,
                       ),
-                      Container(
+                      SizedBox(
                         width: 15.303778648376465,
                         height: 16.67475128173828,
                       ),
-                      Container(
+                      SizedBox(
                         width: 7.349545955657959,
                         height: 11.83403205871582,
                       ),
-                      Container(
+                      SizedBox(
                         width: 8.17718505859375,
                         height: 13.189891815185547,
                       ),
-                      Container(
+                      SizedBox(
                         width: 5.843785762786865,
                         height: 8.427390098571777,
                       ),
-                      Container(
+                      SizedBox(
                         width: 8.17718505859375,
                         height: 13.176136016845703,
                       ),
-                      Container(
+                      SizedBox(
                         width: 10.432770729064941,
                         height: 12.299346923828125,
                       ),
-                      Container(
+                      SizedBox(
                         width: 15.789054870605469,
                         height: 16.703317642211914,
                       ),
                       Stack(
                         children: [
-                          Container(
+                          SizedBox(
                             width: 11.684926986694336,
                             height: 16.732606887817383,
                           ),
-                          Container(
+                          SizedBox(
                             width: 3.9737703800201416,
                             height: 6.919328689575195,
                           ),
@@ -106,30 +106,72 @@ class _AuthSignUPState extends State<AuthSignUP> {
               ),
             ],
           ),
-          Text(
-            "Please enter you email & password to create an account ",
-            style: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
+          const SizedBox(
+            width: 8.17718505859375,
+            height: 13.176136016845703,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(25.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Please enter you email & password to create an",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Text(
+                    "account ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
             ),
+          ),
+          const SizedBox(
+            width: 8.17718505859375,
+            height: 13.176136016845703,
+          ),
+          const SizedBox(
+            width: 8.17718505859375,
+            height: 13.176136016845703,
+          ),
+          const SizedBox(
+            width: 8.17718505859375,
+            height: 13.176136016845703,
           ),
           Stack(
             children: [
-              Text(
+              const Text(
                 "Your Email",
                 style: TextStyle(
                   fontSize: 14,
                   fontWeight: FontWeight.w600,
                 ),
               ),
-              Text(
-                "someuser@gmail.com",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
+              const SizedBox(
+                width: 8.17718505859375,
+                height: 13.176136016845703,
               ),
-              Container(
+              const SizedBox(
+                width: 8.17718505859375,
+                height: 13.176136016845703,
+              ),
+              // const Text(
+              //   "someuser@gmail.com",
+              //   style: TextStyle(
+              //     fontSize: 16,
+              //     fontWeight: FontWeight.w600,
+              //   ),
+              // ),
+              const SizedBox(
                 width: 378,
                 height: 0,
               ),
@@ -143,7 +185,7 @@ class _AuthSignUPState extends State<AuthSignUP> {
                     ),
                   ),
                   Image.asset(
-                    "assets/Vector 3.png",
+                    "assets/images/Message_light.png",
                     width: 17.333332061767578,
                     height: 4.3333330154418945,
                   ),
@@ -151,7 +193,7 @@ class _AuthSignUPState extends State<AuthSignUP> {
               ),
             ],
           ),
-          Stack(
+          const Stack(
             children: [
               Text(
                 "Create Password",
@@ -160,13 +202,13 @@ class _AuthSignUPState extends State<AuthSignUP> {
                   fontWeight: FontWeight.w600,
                 ),
               ),
-              Container(
+              SizedBox(
                 width: 378,
                 height: 0,
               ),
               Column(
                 children: [
-                  Container(
+                  SizedBox(
                     width: 23.833328247070312,
                     height: 20.583332061767578,
                   ),
@@ -174,43 +216,43 @@ class _AuthSignUPState extends State<AuthSignUP> {
               ),
               Stack(
                 children: [
-                  Container(
+                  SizedBox(
                     width: 7,
                     height: 7,
                   ),
-                  Container(
+                  SizedBox(
                     width: 7,
                     height: 7,
                   ),
-                  Container(
+                  SizedBox(
                     width: 7,
                     height: 7,
                   ),
-                  Container(
+                  SizedBox(
                     width: 7,
                     height: 7,
                   ),
-                  Container(
+                  SizedBox(
                     width: 7,
                     height: 7,
                   ),
-                  Container(
+                  SizedBox(
                     width: 7,
                     height: 7,
                   ),
-                  Container(
+                  SizedBox(
                     width: 7,
                     height: 7,
                   ),
-                  Container(
+                  SizedBox(
                     width: 7,
                     height: 7,
                   ),
-                  Container(
+                  SizedBox(
                     width: 7,
                     height: 7,
                   ),
-                  Container(
+                  SizedBox(
                     width: 7,
                     height: 7,
                   ),
@@ -218,7 +260,7 @@ class _AuthSignUPState extends State<AuthSignUP> {
               ),
             ],
           ),
-          Stack(
+          const Stack(
             children: [
               Text(
                 "Already have an account?",
@@ -238,15 +280,15 @@ class _AuthSignUPState extends State<AuthSignUP> {
           ),
           Stack(
             children: [
-              Container(
+              const SizedBox(
                 width: 117,
                 height: 0,
               ),
-              Container(
+              const SizedBox(
                 width: 117,
                 height: 0,
               ),
-              Text(
+              const Text(
                 "or continue with",
                 style: TextStyle(
                   fontSize: 16,
@@ -262,11 +304,11 @@ class _AuthSignUPState extends State<AuthSignUP> {
                       borderRadius: BorderRadius.circular(17),
                     ),
                   ),
-                  Column(
+                  const Column(
                     children: [
                       Stack(
                         children: [
-                          Container(
+                          SizedBox(
                             width: 26,
                             height: 30,
                           ),
@@ -285,17 +327,17 @@ class _AuthSignUPState extends State<AuthSignUP> {
                       borderRadius: BorderRadius.circular(17),
                     ),
                   ),
-                  Column(
+                  const Column(
                     children: [
                       Stack(
                         children: [
                           Stack(
                             children: [
-                              Container(
+                              SizedBox(
                                 width: 30,
                                 height: 29.817768096923828,
                               ),
-                              Container(
+                              SizedBox(
                                 width: 12.832032203674316,
                                 height: 24.140625,
                               ),
@@ -316,25 +358,25 @@ class _AuthSignUPState extends State<AuthSignUP> {
                       borderRadius: BorderRadius.circular(17),
                     ),
                   ),
-                  Column(
+                  const Column(
                     children: [
-                      Container(
+                      SizedBox(
                         width: 13.20000171661377,
                         height: 12.91874885559082,
                       ),
-                      Container(
+                      SizedBox(
                         width: 21.387493133544922,
                         height: 11.13749885559082,
                       ),
-                      Container(
+                      SizedBox(
                         width: 6.056249141693115,
                         height: 12.350000381469727,
                       ),
-                      Container(
+                      SizedBox(
                         width: 21.487504959106445,
                         height: 11.13749885559082,
                       ),
-                      Container(
+                      SizedBox(
                         width: 27.4999942779541,
                         height: 27.4999942779541,
                       ),
@@ -351,18 +393,18 @@ class _AuthSignUPState extends State<AuthSignUP> {
                 height: 20,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(3),
-                  color: Color(0xff2111ad),
+                  color: const Color(0xff2111ad),
                 ),
               ),
-              Column(
+              const Column(
                 children: [
-                  Container(
+                  SizedBox(
                     width: 5.958334445953369,
                     height: 7.041665077209473,
                   ),
                 ],
               ),
-              Text(
+              const Text(
                 "I agree to Chatbot_AI  Public Agreement, Terms, & Privacy Policy.",
                 style: TextStyle(
                   fontSize: 14,
@@ -378,10 +420,10 @@ class _AuthSignUPState extends State<AuthSignUP> {
                 height: 60,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(17),
-                  color: Color(0xff2111ad),
+                  color: const Color(0xff2111ad),
                 ),
               ),
-              Text(
+              const Text(
                 "Continue",
                 style: TextStyle(
                   fontSize: 24,
