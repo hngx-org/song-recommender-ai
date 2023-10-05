615a929e64e69bd08fbfa7a2e001d2529b601c4f
MrazTevin <millatevin@gmail.com>
2023-10-06 00:07:41 +0300

    feat: add some assets and display splash screen

--- lib/utils/widgets/splash_screen.dart
+++ lib/utils/widgets/splash_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:song_recommender_ai/utils/res/colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,34 +11,13 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animationX;
-  late Animation<double> _animationY;
+class _SplashScreenState extends State<SplashScreen> {
+  late Timer _timer;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    )..repeat(reverse: true);
-    _animationX = Tween<double>(
-      begin: -1.0,
-      end: 1.0,
-    ).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    ));
-    _animationY = Tween<double>(
-      begin: -1.0,
-      end: 1.0,
-    ).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    ));
-    Future.delayed(const Duration(seconds: 3), () async {
+    _timer = Timer(const Duration(seconds: 5), () async {
       SharedPreferences sp = await SharedPreferences.getInstance();
       final userId = sp.getString('userId');
       if (!context.mounted) return;
@@ -50,83 +31,28 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override
   void dispose() {
-    _controller.dispose();
+    _timer.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Container(
-              color: Colors.white,
-              child: const Center(
-                child: Text(
-                  'SongScript AI',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: SvgPicture.asset(
+              'assets/images/Frame.svg',
+              fit: BoxFit.contain,
+              semanticsLabel: 'SongScript AI',
             ),
           ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: AnimatedBuilder(
-              animation: _controller,
-              builder: (context, child) {
-                return Transform.translate(
-                  offset: Offset(
-                    MediaQuery.of(context).size.width *
-                        _animationX.value *
-                        0.5,
-                    MediaQuery.of(context).size.height *
-                        _animationY.value *
-                        0.5,
-                  ),
-                  child: child,
-                );
-              },
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Dot(),
-                  Dot(),
-                  Dot(),
-                  Dot(),
-                  Dot(),
-                  Dot(),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Dot extends StatelessWidget {
-  const Dot({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: Container(
-        height: 20,
-        width: 20,
-        decoration: BoxDecoration(
-          color: AppColors.userWidgetColor,
-          borderRadius: BorderRadius.circular(10),
         ),
       ),
+      
     );
   }
 }
